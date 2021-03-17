ced2d23723b65a12af0af366072667cf0f9796bb
Нурислам Каленов <nurislam.kalenov@samgau.com>
2021-03-17 14:32:58 +0600

    init

---
+++ src/test/java/kz/nuris/cardgame/CardgameApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nuris.cardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
